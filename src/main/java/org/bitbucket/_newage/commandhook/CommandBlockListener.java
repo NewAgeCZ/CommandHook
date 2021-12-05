389f57148b52c2530b19b1c132cfe86187032271
_NewAge <newge2@seznam.cz>
2021-12-05 15:21:28 +0100

    Reindent with spaces

--- src/main/java/org/bitbucket/_newage/commandhook/CommandBlockListener.java
+++ src/main/java/org/bitbucket/_newage/commandhook/CommandBlockListener.java
@@ -13,127 +13,127 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
 
 public class CommandBlockListener implements Listener {
-	
-	private final RefUtil refUtil;
-	
-	public CommandBlockListener(RefUtil util) {
-		refUtil = util;
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void onCommandBlockDispatch(ServerCommandEvent e) {
-		if (e.getSender() instanceof BlockCommandSender) {
-			//CommandBlock cmdBlock = ((CommandBlock)((BlockCommandSender)e.getSender()).getBlock().getState());
-			String cmd = e.getCommand();
-			if (cmd.startsWith("/")) {
-				cmd = cmd.replaceFirst("/", "");
-			}
-			
-			if (cmd.startsWith("minecraft:")) {
-				return;
-			}
 
-			String[] args = cmd.split(" ");
-			for(String arg : args) {
-				if(arg.startsWith("@")) {
-					//System.out.println(arg);
-					//System.out.println(arg.substring(0,2));
-					switch(arg.substring(0, 2)) {
-					case "@a":
-					case "@r":
-					case "@s":
-					case "@e":
-					case "@p":
-						List<org.bukkit.entity.Entity> entities = refUtil.parse(arg, ((BlockCommandSender)e.getSender()).getBlock());
-						/*
-						if(entities.isEmpty()) {
-							System.out.println("Empty result");
-							return;
-						}
-						*/
+    private final RefUtil refUtil;
 
-						for(Iterator<org.bukkit.entity.Entity> i = entities.iterator(); i.hasNext();) {
-							org.bukkit.entity.Entity o = i.next();
-							if(o == null) continue;
-							Bukkit.dispatchCommand(e.getSender(), cmd.replace(arg,o.getName()));
-							//System.out.println(o.getName());
-						}
-						e.setCancelled(true);
-						//callArgumentParser(arg, ((BlockCommandSender)e.getSender()).getBlock());
-						//PlayerSelector s = new PlayerSelector(cmdBlock, arg);
-						//s.getArgs();
-						break;
-					
-						//RandomSelector r = new RandomSelector(cmdBlock, arg);
-						//e.setCommand(cmd.replace(arg, r.randomPlayer().toString()));
-						//Bukkit.dispatchCommand(e.getSender(), cmd.replace(arg, r.randomPlayer().toString()));
-					default:
-						return;
-					}
-				}
-			}
-		}
-	}
-	
-	/*
-	private void callArgumentParser(String arg, Block cmdBlock) {
-		try {
-			//Class SRClass = Class.forName("com.mojang.brigadier.StringReader");
-			Class TileCommandClass = Class.forName("net.minecraft.server.v1_13_R2.TileEntityCommand");
-			Class APSelClass = Class.forName("net.minecraft.server.v1_13_R2.ArgumentParserSelector");
-			Constructor APConst = APSelClass.getConstructor(StringReader.class);
-			
-			CraftCommandBlock ccb = new CraftCommandBlock(cmdBlock);
-			TileEntityCommand tec = (TileEntityCommand)((CraftWorld)cmdBlock.getWorld()).getTileEntityAt(cmdBlock.getX(), cmdBlock.getY(), cmdBlock.getZ());//(TileEntityCommand)TileCommandClass.cast(ccb);
-			//Field h = tec.getClass().getDeclaredField("h");
-			//h.setAccessible(true);
-			//Object hInstance = h.get(tec.getComm);
-			
-			CommandBlockListenerAbstract cbla = tec.getCommandBlock();//(CommandBlockListenerAbstract)(Class.forName("net.minecraft.server.v1_13_R2.CommandBlockListenerAbstract").cast(hInstance));
-			CommandListenerWrapper clw = cbla.getWrapper();
-			
-			//Object stringReaderInstance =  SRConst.newInstance(arg);
-			//StringReader stringReader = (StringReader)SRClass.cast(stringReaderInstance);
-			StringReader stringReader = new StringReader(arg.substring(1));
-			ArgumentParserSelector APSelector = (ArgumentParserSelector)APSelClass.cast(APConst.newInstance(stringReader));
-			
-			//APSelector.b();
-			Method b = APSelector.getClass().getDeclaredMethod("b");
-			b.setAccessible(true);
-			b.invoke(APSelector);
-			
-			EntitySelector es = APSelector.a();//APSelector.s();
-			List<? extends Entity> l = es.b(clw);
-			if(!l.isEmpty())
-				for(Iterator<? extends Entity> i = l.iterator(); i.hasNext();) {
-					Entity ent = i.next();
-					System.out.print(ent.toString());
-				}
-			else System.out.println("No target found");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CommandSyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	*/
+    public CommandBlockListener(RefUtil util) {
+        refUtil = util;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onCommandBlockDispatch(ServerCommandEvent e) {
+        if (e.getSender() instanceof BlockCommandSender) {
+            //CommandBlock cmdBlock = ((CommandBlock)((BlockCommandSender)e.getSender()).getBlock().getState());
+            String cmd = e.getCommand();
+            if (cmd.startsWith("/")) {
+                cmd = cmd.replaceFirst("/", "");
+            }
+
+            if (cmd.startsWith("minecraft:")) {
+                return;
+            }
+
+            String[] args = cmd.split(" ");
+            for(String arg : args) {
+                if(arg.startsWith("@")) {
+                    //System.out.println(arg);
+                    //System.out.println(arg.substring(0,2));
+                    switch(arg.substring(0, 2)) {
+                        case "@a":
+                        case "@r":
+                        case "@s":
+                        case "@e":
+                        case "@p":
+                            List<org.bukkit.entity.Entity> entities = refUtil.parse(arg, ((BlockCommandSender)e.getSender()).getBlock());
+                        /*
+                        if(entities.isEmpty()) {
+                            System.out.println("Empty result");
+                            return;
+                        }
+                        */
+
+                            for(Iterator<org.bukkit.entity.Entity> i = entities.iterator(); i.hasNext();) {
+                                org.bukkit.entity.Entity o = i.next();
+                                if(o == null) continue;
+                                Bukkit.dispatchCommand(e.getSender(), cmd.replace(arg,o.getName()));
+                                //System.out.println(o.getName());
+                            }
+                            e.setCancelled(true);
+                            //callArgumentParser(arg, ((BlockCommandSender)e.getSender()).getBlock());
+                            //PlayerSelector s = new PlayerSelector(cmdBlock, arg);
+                            //s.getArgs();
+                            break;
+
+                        //RandomSelector r = new RandomSelector(cmdBlock, arg);
+                        //e.setCommand(cmd.replace(arg, r.randomPlayer().toString()));
+                        //Bukkit.dispatchCommand(e.getSender(), cmd.replace(arg, r.randomPlayer().toString()));
+                        default:
+                            return;
+                    }
+                }
+            }
+        }
+    }
+    
+    /*
+    private void callArgumentParser(String arg, Block cmdBlock) {
+        try {
+            //Class SRClass = Class.forName("com.mojang.brigadier.StringReader");
+            Class TileCommandClass = Class.forName("net.minecraft.server.v1_13_R2.TileEntityCommand");
+            Class APSelClass = Class.forName("net.minecraft.server.v1_13_R2.ArgumentParserSelector");
+            Constructor APConst = APSelClass.getConstructor(StringReader.class);
+            
+            CraftCommandBlock ccb = new CraftCommandBlock(cmdBlock);
+            TileEntityCommand tec = (TileEntityCommand)((CraftWorld)cmdBlock.getWorld()).getTileEntityAt(cmdBlock.getX(), cmdBlock.getY(), cmdBlock.getZ());//(TileEntityCommand)TileCommandClass.cast(ccb);
+            //Field h = tec.getClass().getDeclaredField("h");
+            //h.setAccessible(true);
+            //Object hInstance = h.get(tec.getComm);
+            
+            CommandBlockListenerAbstract cbla = tec.getCommandBlock();//(CommandBlockListenerAbstract)(Class.forName("net.minecraft.server.v1_13_R2.CommandBlockListenerAbstract").cast(hInstance));
+            CommandListenerWrapper clw = cbla.getWrapper();
+            
+            //Object stringReaderInstance =  SRConst.newInstance(arg);
+            //StringReader stringReader = (StringReader)SRClass.cast(stringReaderInstance);
+            StringReader stringReader = new StringReader(arg.substring(1));
+            ArgumentParserSelector APSelector = (ArgumentParserSelector)APSelClass.cast(APConst.newInstance(stringReader));
+            
+            //APSelector.b();
+            Method b = APSelector.getClass().getDeclaredMethod("b");
+            b.setAccessible(true);
+            b.invoke(APSelector);
+            
+            EntitySelector es = APSelector.a();//APSelector.s();
+            List<? extends Entity> l = es.b(clw);
+            if(!l.isEmpty())
+                for(Iterator<? extends Entity> i = l.iterator(); i.hasNext();) {
+                    Entity ent = i.next();
+                    System.out.print(ent.toString());
+                }
+            else System.out.println("No target found");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (CommandSyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    */
 }
