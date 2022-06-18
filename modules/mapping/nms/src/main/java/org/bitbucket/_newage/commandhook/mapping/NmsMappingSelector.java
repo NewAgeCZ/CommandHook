3bad3f46d7aea0ff25b7b69389d235313b912fe2
Václav Plic <plic.vasek@gmail.com>
2022-06-18 15:13:58 +0200

    Add support for 1.19

--- modules/mapping/nms/src/main/java/org/bitbucket/_newage/commandhook/mapping/NmsMappingSelector.java
+++ modules/mapping/nms/src/main/java/org/bitbucket/_newage/commandhook/mapping/NmsMappingSelector.java
@@ -65,6 +65,10 @@ public class NmsMappingSelector {
                 mapping = new NmsV1_18_R2();
                 break;
 
+            case "v1_19_R1":
+                mapping = new NmsV1_19_R1();
+                break;
+
             default:
                 logger.warn("Mapping for {} not found! Either the plugin is outdated or has not been updated yet!", nmsVersion);
                 logger.info("Falling back to legacy mode");
