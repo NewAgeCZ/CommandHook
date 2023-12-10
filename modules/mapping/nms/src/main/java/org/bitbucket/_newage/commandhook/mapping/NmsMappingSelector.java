37f65e30ff36c1d6e8106986f89da96f7d6744db
Václav Plic <plic.vasek@gmail.com>
2023-12-10 18:08:06 +0100

    Release 2.4.2 - Add support for 1.20.4

--- modules/mapping/nms/src/main/java/org/bitbucket/_newage/commandhook/mapping/NmsMappingSelector.java
+++ modules/mapping/nms/src/main/java/org/bitbucket/_newage/commandhook/mapping/NmsMappingSelector.java
@@ -21,7 +21,10 @@ public class NmsMappingSelector {
      *
      * @param nmsVersion e.g. v1_16_R3
      * @return implementation of {@link IMapping}
+     * @deprecated Paper will provide only jars without relocation -> no more relocation shenanigans.
+     *             Use Minecraft version instead
      */
+    @Deprecated
     public static IMapping fromNmsVersion(String nmsVersion) {
         IMapping mapping;
         switch (nmsVersion) {
@@ -89,6 +92,97 @@ public class NmsMappingSelector {
                 logger.warn("Mapping for {} not found! Either the plugin is outdated or has not been updated yet!", nmsVersion);
                 logger.info("Falling back to legacy mode");
                 mapping = new LegacyMapping(nmsVersion);
+        }
+
+        return mapping;
+    }
+
+    public static IMapping fromMinecraftVersion(String minecraftVersion) {
+        IMapping mapping;
+        switch (minecraftVersion) {
+        case "1.13":
+            mapping = new NmsV1_13_R1();
+            break;
+
+        case "1.13.1":
+        case "1.13.2":
+            mapping = new NmsV1_13_R2();
+            break;
+
+        case "1.14":
+        case "1.14.1":
+        case "1.14.2":
+        case "1.14.3":
+        case "1.14.4":
+            mapping = new NmsV1_14();
+            break;
+
+        case "1.15":
+        case "1.15.1":
+        case "1.15.2":
+            mapping = new NmsV1_15();
+            break;
+
+        case "1.16.1":
+            mapping = new NmsV1_16_R1();
+            break;
+
+        case "1.16.2":
+        case "1.16.3":
+            mapping = new NmsV1_16_R2();
+            break;
+
+        case "1.16.4":
+        case "1.16.5":
+            mapping = new NmsV1_16_R3();
+            break;
+
+        case "1.17":
+        case "1.17.1":
+            mapping = new NmsV1_17();
+            break;
+
+        case "1.18":
+        case "1.18.1":
+            mapping = new NmsV1_18_R1();
+            break;
+
+        case "1.18.2":
+            mapping = new NmsV1_18_R2();
+            break;
+
+        case "1.19":
+        case "1.19.1":
+        case "1.19.2":
+            mapping = new NmsV1_19_R1();
+            break;
+
+        case "1.19.3":
+            mapping = new NmsV1_19_R2();
+            break;
+
+        case "1.19.4":
+            mapping = new NmsV1_19_R3();
+            break;
+
+        case "1.20":
+        case "1.20.1":
+            mapping = new NmsV1_20_R1();
+            break;
+
+        case "1.20.2":
+            mapping = new NmsV1_20_R2();
+            break;
+
+        case "1.20.3":
+        case "1.20.4":
+            mapping = new NmsV1_20_R3();
+            break;
+
+        default:
+            logger.warn("Mapping for {} not found! Either the plugin is outdated or has not been updated yet!", minecraftVersion);
+            logger.info("Falling back to legacy mode");
+            mapping = new LegacyMapping(minecraftVersion);
         }
 
         return mapping;
